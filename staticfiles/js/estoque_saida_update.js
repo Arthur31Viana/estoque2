52c34b98811aace20af51606d26f80411c4df4ea
rg3915 <regis42santos@gmail.com>
2024-09-10 00:41:23 -0300

    Pega os dados do produto.

--- staticfiles/js/estoque_saida_update.js
+++ staticfiles/js/estoque_saida_update.js
@@ -10,10 +10,11 @@ const getEstoque = () => ({
   items: [],
   produtos: [],
   editItem: {},
+  saldo: 0,
 
   init() {
     this.getData()
-    this.getProduto()
+    this.getProdutos()
   },
 
   getData() {
@@ -24,28 +25,36 @@ const getEstoque = () => ({
       })
   },
 
-  getProduto() {
+  getProdutos() {
     const url = `${baseUrl}/produtos`
     axios.get(url)
       .then(response => {
         this.produtos = response.data
+        console.log(response.data.length)
       })
   },
 
-  updateItem(item) {
+  getProduto(item) {
+    console.log(item.produto)
+    console.table(item)
     // Pega a estoque atual do produto.
     const url_produto = `${baseUrl}/produtos/${item.produto}`
-
     axios.get(url_produto)
       .then(response => {
         item.saldo = response.data.estoque - item.quantidade
       })
+  },
 
+  updateItem(item) {
     // Edita o item.
     const url = `${baseUrl}/estoque-saida-item/${item.id}`
 
     // Remove o id, e envia o restante do item para o backend.
     const { id: _, produto: produto_id, ...bodyData } = item
+
+    // bodyData.saldo = this.saldo
+
+    console.log(bodyData)
 
     // Renomeia produto para produto_id.
     bodyData.produto_id = parseInt(produto_id)
