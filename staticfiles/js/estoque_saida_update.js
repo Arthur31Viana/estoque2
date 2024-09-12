cc03374fe897547e064533f02ac4ba508da72e47
rg3915 <regis42santos@gmail.com>
2024-09-12 20:57:05 -0300

    Retorna o saldo correto e salva no banco de dados.

--- staticfiles/js/estoque_saida_update.js
+++ staticfiles/js/estoque_saida_update.js
@@ -8,9 +8,9 @@ const baseUrl = '/api/v1'
 
 const getEstoque = () => ({
   items: [],
+  editItem: {},
   produtos: [],
   editItem: {},
-  saldo: 0,
 
   init() {
     this.getData()
@@ -21,7 +21,9 @@ const getEstoque = () => ({
     const url = `${baseUrl}/estoque-saida/${pk}`
     axios.get(url)
       .then(response => {
-        this.items = response.data
+        this.items = response.data.map(item => {
+          return { ...item, errorMessage: '' }
+        })
       })
   },
 
@@ -30,18 +32,16 @@ const getEstoque = () => ({
     axios.get(url)
       .then(response => {
         this.produtos = response.data
-        console.log(response.data.length)
       })
   },
 
   getProduto(item) {
-    console.log(item.produto)
-    console.table(item)
     // Pega a estoque atual do produto.
     const url_produto = `${baseUrl}/produtos/${item.produto}`
     axios.get(url_produto)
       .then(response => {
-        item.saldo = response.data.estoque - item.quantidade
+        item.estoque_atual = response.data.estoque
+        item.saldo_atual = item.estoque_atual - item.quantidade
       })
   },
 
@@ -50,17 +50,21 @@ const getEstoque = () => ({
     const url = `${baseUrl}/estoque-saida-item/${item.id}`
 
     // Remove o id, e envia o restante do item para o backend.
-    const { id: _, produto: produto_id, ...bodyData } = item
+    // Remove também o produto porque ele está como um objeto,
+    // e precisamos enviar apenas produto_id.
+    const { id: _, produto: produto_nao_usado, ...bodyData } = item
 
-    // bodyData.saldo = this.saldo
+    bodyData.saldo = item.produto.estoque - item.quantidade
 
-    console.log(bodyData)
+    // Deve ser enviado o campo com o nome produto_id, pois é isso que o
+    // schema do Django está esperando.
+    bodyData.produto_id = produto_nao_usado.id
 
-    // Renomeia produto para produto_id.
-    bodyData.produto_id = parseInt(produto_id)
+    item.errorMessage = ''
 
     axios.patch(url, bodyData, { headers })
       .then(response => console.log(response.data))
+      .catch(error => item.errorMessage = error.response.data.detail)
   },
 
 })
